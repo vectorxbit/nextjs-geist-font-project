84b02eca86ca3d50f47d70646c0048e5e0721966
vectorxbit <vectormygun@gmail.com>
2025-07-13 09:49:45 +0700

    Add .next/static/chunks/src_app_stock_page_tsx_0ef0cac3._.js 1752374985321

---
+++ .next/static/chunks/src_app_stock_page_tsx_0ef0cac3._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/_9f7d466d._.js"
+],
+    source: "dynamic"
+});
